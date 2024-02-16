c7885c016dfc69d155f3751d2869715b715c8ae4
ToffiCodes <tevfik.kohl@gmail.com>
2024-02-16 14:25:16 +0100

    task 5 begin

---
+++ CodingTaskFive/script.js
@@ -0,0 +1,41 @@
+const size = 25;
+let result;
+
+if (size > 10) {
+  result = "Greater than 10";
+} else if (size > 20) {
+  result = "Greater than 20";
+} else {
+  result = "Lower than 10";
+}
+
+console.log(result);
+// Returns Greater than 10 but should return Greater than 20
+
+// TODO: Implement the oddEven function
+
+console.log(oddEven(4));
+// result should be even
+
+console.log(oddEven(3));
+// result should be odd
+
+console.log(oddEven(-1));
+// result should be odd
+
+console.log(oddEven(10));
+// result should be even
+
+// TODO: Implement the oldYoung function
+
+console.log(oldYoung(27));
+// result should be young person
+
+console.log(oldYoung(6));
+// result should be children
+
+console.log(oldYoung(-1));
+// result should be invalid parameter
+
+console.log(oldYoung(86));
+// result should be elder person
