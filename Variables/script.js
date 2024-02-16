405ca10972cca8f32396ec0affdbbbfb8be8cea7
ToffiCodes <tevfik.kohl@gmail.com>
2024-02-16 14:20:47 +0100

    task done

---
+++ Variables/script.js
@@ -0,0 +1,30 @@
+let userName = "Brad";
+userName = "Jenna";
+
+function getUserNameLength(name) {
+  return name.length;
+}
+
+console.log(getUserNameLength(userName) > 4);
+// ^______________ Should log true
+
+// TODO: Implement the isString function
+
+const isString = function (input) {
+  return typeof input === "string";
+};
+
+console.log(isString("Hello"));
+// result should be true
+
+console.log(isString(3));
+// result should be false
+
+console.log(isString(undefined));
+// result should be false
+
+console.log(isString(""));
+// result should be true
+
+console.log(isString("John" + "Doe"));
+// result should be true
