f2e82785b5138f44d2e1055843e86d8d1c871090
ToffiCodes <tevfik.kohl@gmail.com>
2024-02-16 14:28:46 +0100

    task 6 begin

---
+++ CodingTaskSix/script.js
@@ -0,0 +1,21 @@
+// TODO: Implement the oddNumbers function
+
+console.log(oddNumbers(0, 4));
+// result should be: 1,3
+
+console.log(oddNumbers(10, 33));
+// result should be: 11,13,15,17,19,21,23,25,27,29,31,33
+
+console.log(oddNumbers(9, 12));
+// result should be: 9,11
+
+// TODO: Implement the charCount function
+
+console.log(charCount("hello", "l"));
+// result should be: 2
+
+console.log(charCount("mama", "m"));
+// result should be: 2
+
+console.log(charCount("Resümee", "e"));
+// result should be: 3
